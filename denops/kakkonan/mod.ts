c93cb6dfcdd9807c976aeadb1d14ad447567d94a
higashi000 <higashi.ict@gmail.com>
2021-02-26 00:07:31 +0900

    Update refactoring

--- denops/kakkonan/mod.ts
+++ denops/kakkonan/mod.ts
@@ -12,27 +12,38 @@ const brackets: { [name: string]: string} = {
 const backQuote = '`';
 
 start(async (vim) => {
+    const getLineChar = async (diff: number): Promise<string> => {
+        if (typeof diff !== "number") {
+            throw new Error(`'diff' attribute of 'getLineChar' in must be a number`)
+        }
+
+        const corsorStr = await vim.call('getline', '.');
+        if (typeof corsorStr !== "string") {
+            throw new Error(`'corsorStr' attribute of 'kakkonanCompletion' in must be a string`)
+        }
+
+        const corsorLine = await vim.call('line', '.');
+        if (typeof corsorLine !== "number") {
+            throw new Error(`'corsorLine' attribute of 'kakkonanCompletion' in must be a number`)
+        }
+
+        const corsorCol = await vim.call('col', '.');
+        if (typeof corsorCol !== "number") {
+            throw new Error(`'corsorCol' attribute of 'kakkonanCompletion' in must be a number`)
+        }
+
+        const corsorChar = corsorStr.substr(corsorCol + diff, 1);
+
+        return corsorChar;
+    }
+
     vim.register({
         async kakkonanCompletion(inputBrackets: unknown): Promise<string> {
             if (typeof inputBrackets !== "string") {
                 throw new Error(`'inputBrackets' attribute of 'kakkonanCompletion' in must be a string`)
             }
-            const corsorStr = await vim.call('getline', '.');
-            if (typeof corsorStr !== "string") {
-                throw new Error(`'corsorStr' attribute of 'kakkonanCompletion' in must be a string`)
-            }
 
-            const corsorLine = await vim.call('line', '.');
-            if (typeof corsorLine !== "number") {
-                throw new Error(`'corsorLine' attribute of 'kakkonanCompletion' in must be a number`)
-            }
-
-            const corsorCol = await vim.call('col', '.');
-            if (typeof corsorCol !== "number") {
-                throw new Error(`'corsorCol' attribute of 'kakkonanCompletion' in must be a number`)
-            }
-
-            const corsorChar = corsorStr.substr(corsorCol - 1, 1);
+            const corsorChar = await getLineChar(-1);
 
             if (inputBrackets == '"' || inputBrackets == "'" || inputBrackets == "`") {
                 if (corsorChar == inputBrackets) {
@@ -49,75 +60,45 @@ start(async (vim) => {
 
         async kakkonanEscapeBrackets(inputBracket: unknown): Promise<boolean> {
             if (typeof inputBracket !== "string") {
-                throw new Error(`'inputBrackets' attribute of 'kakkonanEscapeBrackets' in must be a string`)
+                throw new Error(`'inputBrackets' attribute of 'kakkonanEscapeBrackets' in must be a string`);
             }
 
-            const corsorStr = await vim.call('getline', '.');
-            if (typeof corsorStr !== "string") {
-                throw new Error(`'corsorStr' attribute of 'kakkonanCompletion' in must be a string`)
-            }
-
-            const corsorLine = await vim.call('line', '.');
-            if (typeof corsorLine !== "number") {
-                throw new Error(`'corsorLine' attribute of 'kakkonanCompletion' in must be a number`)
-            }
-
-            const corsorCol = await vim.call('col', '.');
-            if (typeof corsorCol !== "number") {
-                throw new Error(`'corsorCol' attribute of 'kakkonanCompletion' in must be a number`)
-            }
-
-            const corsorChar = corsorStr.substr(corsorCol - 1, 1);
+            const corsorChar = await getLineChar(-1);
 
             if (corsorChar == inputBracket) {
-                return true
+                return true;
             }
 
-            return false
+            return false;
         },
 
         async kakkonanBackSpaceEnter(): Promise<boolean> {
-            const corsorStr = await vim.call('getline', '.');
-            if (typeof corsorStr !== "string") {
-                throw new Error(`'corsorStr' attribute of 'kakkonanCompletion' in must be a string`)
-            }
-
-            const corsorLine = await vim.call('line', '.');
-            if (typeof corsorLine !== "number") {
-                throw new Error(`'corsorLine' attribute of 'kakkonanCompletion' in must be a number`)
-            }
-
-            const corsorCol = await vim.call('col', '.');
-            if (typeof corsorCol !== "number") {
-                throw new Error(`'corsorCol' attribute of 'kakkonanCompletion' in must be a number`)
-            }
-
-            const corsorRight = corsorStr.substr(corsorCol - 1, 1);
-            const corsorChar = corsorStr.substr(corsorCol - 2, 1);
+            const corsorRight =  await getLineChar(-1);
+            const corsorChar = await getLineChar(-2);
 
             if (brackets[corsorChar] && brackets[corsorChar] == corsorRight) {
-                return true
+                return true;
             }
 
-            return false
+            return false;
         },
     })
 
     // completion brackets and some quote
-    vim.execute(`inoremap <expr> ( denops#request("kakkonan", "kakkonanCompletion", ['(']) . "\<left>"`)
-    vim.execute(`inoremap <expr> { denops#request("kakkonan", "kakkonanCompletion", ['{']) . "\<left>"`)
-    vim.execute(`inoremap <expr> [ denops#request("kakkonan", "kakkonanCompletion", ['[']) . "\<left>"`)
-    vim.execute(`inoremap <expr> " denops#request("kakkonan", "kakkonanCompletion", ['"']) != "" ? '""' . "\<left>" : "\<right>"`)
-    vim.execute(`inoremap <expr> ' denops#request("kakkonan", "kakkonanCompletion", ["'"]) != "" ? "''" . "\<left>" : "\<right>"`)
-    vim.execute(`inoremap <expr> ${backQuote} denops#request("kakkonan", "kakkonanCompletion", ['${backQuote}']) != "" ? '${backQuote + backQuote}' . "\<left>" : "\<right>"`)
+    vim.execute(`inoremap <expr> ( denops#request("kakkonan", "kakkonanCompletion", ['(']) . "\<left>"`);
+    vim.execute(`inoremap <expr> { denops#request("kakkonan", "kakkonanCompletion", ['{']) . "\<left>"`);
+    vim.execute(`inoremap <expr> [ denops#request("kakkonan", "kakkonanCompletion", ['[']) . "\<left>"`);
+    vim.execute(`inoremap <expr> " denops#request("kakkonan", "kakkonanCompletion", ['"']) != "" ? '""' . "\<left>" : "\<right>"`);
+    vim.execute(`inoremap <expr> ' denops#request("kakkonan", "kakkonanCompletion", ["'"]) != "" ? "''" . "\<left>" : "\<right>"`);
+    vim.execute(`inoremap <expr> ${backQuote} denops#request("kakkonan", "kakkonanCompletion", ['${backQuote}']) != "" ? '${backQuote + backQuote}' . "\<left>" : "\<right>"`);
     // escape some brackets
-    vim.execute(`inoremap <expr> ) denops#request("kakkonan", "kakkonanEscapeBrackets", [')']) == v:false ? ")" : "\<right>"`)
-    vim.execute(`inoremap <expr> } denops#request("kakkonan", "kakkonanEscapeBrackets", ['}']) == v:false ? "}" : "\<right>"`)
-    vim.execute(`inoremap <expr> ] denops#request("kakkonan", "kakkonanEscapeBrackets", [']']) == v:false ? "]" : "\<right>"`)
+    vim.execute(`inoremap <expr> ) denops#request("kakkonan", "kakkonanEscapeBrackets", [')']) == v:false ? ")" : "\<right>"`);
+    vim.execute(`inoremap <expr> } denops#request("kakkonan", "kakkonanEscapeBrackets", ['}']) == v:false ? "}" : "\<right>"`);
+    vim.execute(`inoremap <expr> ] denops#request("kakkonan", "kakkonanEscapeBrackets", [']']) == v:false ? "]" : "\<right>"`);
 
     // input enter
-    vim.execute(`inoremap <expr> <CR> denops#request("kakkonan", "kakkonanBackSpaceEnter", []) == v:false ? "\<CR>" : "\<CR>\<C-o>\<S-o>"`)
+    vim.execute(`inoremap <expr> <CR> denops#request("kakkonan", "kakkonanBackSpaceEnter", []) == v:false ? "\<CR>" : "\<CR>\<C-o>\<S-o>"`);
 
     // input backspace
-    vim.execute(`inoremap <expr> <BS> denops#request("kakkonan", "kakkonanBackSpaceEnter", []) == v:false ? "\<BS>" : "\<BS>\<right>\<BS>"`)
+    vim.execute(`inoremap <expr> <BS> denops#request("kakkonan", "kakkonanBackSpaceEnter", []) == v:false ? "\<BS>" : "\<BS>\<right>\<BS>"`);
 })
