0772fc12126b3c8a919b94eb57368687f7bfbc0b
higashi000 <higashi.ict@gmail.com>
2021-06-10 21:02:48 +0900

    change: split file

--- denops/kakkonan/kakkonanMod/mod.ts
+++ denops/kakkonan/kakkonanMod/mod.ts
@@ -3,3 +3,4 @@ export * from "./getLineChar.ts";
 export * from "./completion.ts";
 export * from "./escape.ts";
 export * from "./bscr.ts";
+export * from "./surround.ts";
---
+++ denops/kakkonan/kakkonanMod/surround.ts
@@ -0,0 +1,42 @@
+import { Vim } from "https://deno.land/x/denops_std@v0.10/mod.ts";
+import { backQuote, brackets } from "./brackets.ts";
+
+export async function surroundBrackets(
+  vim: Vim,
+  inputBracket: string,
+): Promise<void> {
+  await vim.execute("normal `<");
+
+  const startLineNo = await vim.call("line", ".") as number;
+  const startColNo = await vim.call("col", ".") as number;
+
+  await vim.execute("normal `>");
+
+  const finishLineNo = await vim.call("line", ".") as number;
+  const finishColNo = await vim.call("col", ".") as number;
+
+  const line = await vim.call("getline", ".") as string;
+
+  if (startLineNo != finishLineNo) {
+    const startLine = await vim.call("getline", startLineNo) as string;
+    const finishLine = await vim.call("getline", finishLineNo) as string;
+
+    const updateStartLine = startLine.slice(0, startColNo - 1) +
+      inputBracket + startLine.slice(startColNo - 1, startLine.length);
+    const updateFinishLine = finishLine.slice(0, finishColNo) +
+      brackets[inputBracket] +
+      finishLine.slice(finishColNo, finishLine.length);
+
+    await vim.call("setline", startLineNo, updateStartLine);
+    await vim.call("setline", finishLineNo, updateFinishLine);
+    return;
+  }
+
+  const surroundText = line.slice(0, startColNo - 1) + inputBracket +
+    line.slice(startColNo - 1, finishColNo) +
+    brackets[inputBracket] + line.slice(finishColNo, line.length);
+
+  await vim.call("setline", startLineNo, surroundText);
+
+  return;
+}
