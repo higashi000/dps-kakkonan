983ce1981ec54abe4c93201931f363e949cbe033
higashi000 <higashi.ict@gmail.com>
2021-12-12 23:13:13 +0900

    fix: deno fmt

--- denops/kakkonan/mod/getLineChar.ts
+++ denops/kakkonan/mod/getLineChar.ts
@@ -4,12 +4,16 @@ export async function getLineChar(vim: Denops, diff: number): Promise<string> {
   const cursorStr = await vim.call("getline", ".") as string;
 
   const cursorCol = await vim.call("col", ".") as number;
-  console.log(cursorCol)
+  console.log(cursorCol);
 
   //const cursorChar = cursorStr.substr(cursorCol + diff, 1);
 
-  const cursorChar = await vim.call("matchstr", cursorStr, ".", cursorCol + diff);
-
+  const cursorChar = await vim.call(
+    "matchstr",
+    cursorStr,
+    ".",
+    cursorCol + diff,
+  );
 
   return cursorChar;
 }
